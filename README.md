ec0f42b0cb0b5e9dbd5be2406e79d6d1e0b4fd81
airy <monommino@gmail.com>
2020-11-17 03:39:56 -0600

    Add src_width, src_height and border options

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A `descale` wrapper for anime destriping.
 
 * VapourSynth version R46 or newer
 * [vapoursynth-descale](https://github.com/Irrational-Encoding-Wizardry/vapoursynth-descale)
-* VapourSynth R49 and descale R3 or newer for `Spline64` support.
+* VapourSynth R49 and Descale R3 or newer for `Spline64` support.
+* [EdgeFixer](https://github.com/sekrit-twc/EdgeFixer) for options of fixing top and bottom borders before descaling.
 
 ## How it works
 
@@ -13,31 +14,35 @@ Some striped (aka cross-conversion, or 縞縞) anime artifacts are caused by imp
 
 ## Usage
 
-The first plane will be internally converted to `GRAYS` and be processed. The output has the same format as the first plane of the input.
+The first plane of the input clip will be internally converted to `GRAY16` if fixing borders is requested, then `GRAYS`, and be processed. The output has the same format as the first plane of the input.
 ```python
 from destripe import Destripe
 
-down = Destripe(clip clip[, int width=1280, int height=360, str kernel='bicubic', float b=0, float c=1/2, int taps=3, float[] src_top=[0, 0], float[] src_left=[0, 0], bool showdiff=False])
+down = Destripe(clip clip[, int width=1280, int height=360, str kernel='bicubic', float b=0, float c=1/2, int taps=3, float[] src_left=[0, 0], float[] src_top=[0, 0], float[] src_width=[width, width], float[] src_height=[height, height], int[] fix_top=[0, 0], int[] fix_bottom=[0, 0] bool showdiff=False])
 
 down, diff = Destripe(clip, ..., showdiff=True)
 ```
 Parameters:
 - `width`, `height`</br>
-The destination descale size for each field. For example, if you believe the native resolution was 1280x720, then each field should be 1280x360.
+The destination descaled size for each field. For example, if you believe the native resolution was 1280x720, then each field should be 1280x360.
 - `kernel`, `b`, `c`, `taps`</br>
-Parameters of the resizers. See [descale's page](https://github.com/Irrational-Encoding-Wizardry/vapoursynth-descale#usage).
-- `src_top`, `src_left`</br>
-Specify the cropping for the top field and the bottom field, respectively. They refer to the initial upscale, see [this link](https://github.com/Irrational-Encoding-Wizardry/vapoursynth-descale/issues/2#issuecomment-305876093). Usually you need to modify `src_top` until satisfactory.
+Parameters of the resizers. See [Descale's page](https://github.com/Irrational-Encoding-Wizardry/vapoursynth-descale#usage).
+- `src_left`, `src_top`, `src_width`, `src_height`</br>
+Specify the cropping parameters for the top field and the bottom field, respectively. They refer to the initial upscale, see [this link](https://github.com/Irrational-Encoding-Wizardry/vapoursynth-descale/issues/2#issuecomment-305876093). Usually you need to modify `src_top` until satisfactory.
+Note that `Descale` only accepts positive values of `src_width` and `src_height`, so negative or zero inputs will be replaced by values of `width` and `height`, respectively.
+- `fix_top`, `fix_bottom`</br>
+If specified, `EdgeFixer` will be applied to the top and bottom borders for each field, respectively, before descaling.
 - `showdiff`</br>
 Set to `True` to include the de-rescale absolute error `diff` in the return list. `diff` has the same format as the first plane of `clip`.
 
 Notes:
-- IVTC first if required, since Destripe doesn't deal with field order.
-- Bad borders could be amplified while descaling. You may give [EdgeFixer](https://github.com/sekrit-twc/EdgeFixer) a try before processing.
-- These settings could cause a position shift of the output from the source and you are supposed to fix by yourself.
+- IVTC should be taken prior to destriping, since this script doesn't deal with field order.
+- Bad borders could be amplified while descaling.
+Tweaking `fix_top` and `fix_bottom` may and may not be better than fixing edges before calling `Destripe`.
+- Resizer settings could cause a position shift of the output from the source and you are supposed to fix by yourself.
 
 ## Examples
-Suppose `clip` already has its borders fixed.
+In these examples `clip` already has its borders fixed before processing.
 #### Example 1
 ```python
 down = Destripe(clip, 1280, 360, kernel='spline64', src_top=[1/6, 1/6])
--- destripe.py
+++ destripe.py
@@ -10,23 +10,54 @@ def Destripe(clip: vs.VideoNode,
              b: float = 0,
              c: float = 1/2,
              taps: int = 3,
-             src_top: Union[float, List[float]] = [0.0, 0.0],
              src_left: Union[float, List[float]] = [0.0, 0.0],
+             src_top: Union[float, List[float]] = [0.0, 0.0],
+             src_width: Union[float, List[float]] = [0.0, 0.0],
+             src_height: Union[float, List[float]] = [0.0, 0.0],
+             fix_top: Union[int, List[int]] = [0, 0],
+             fix_bottom: Union[int, List[int]] = [0, 0],
              showdiff: bool = False) -> Union[vs.VideoNode, List[vs.VideoNode]]:
     y = clip if clip.format.color_family == vs.GRAY else core.std.ShufflePlanes(clip, 0, vs.GRAY)
-    isfloat = (y.format.sample_type == vs.FLOAT and y.format.bits_per_sample == 32)
-    y32 = y if isfloat else core.resize.Point(y, format=vs.GRAYS)
     kernel = kernel.lower()
     if kernel.startswith('de'):
         kernel = kernel[2:]
-    if not isinstance(src_top, list):
-        src_top = [src_top, src_top]
     if not isinstance(src_left, list):
         src_left = [src_left, src_left]
-    sep = core.std.SeparateFields(y32, True).std.SetFrameProp('_Field', delete=True)
+    if not isinstance(src_top, list):
+        src_top = [src_top, src_top]
+    if not isinstance(src_width, list):
+        src_width = [src_width, src_width]
+    src_width = [width if v <= 0 else v for v in src_width]
+    if not isinstance(src_height, list):
+        src_height = [src_height, src_height]
+    src_height = [height if v <= 0 else v for v in src_height]
+    if not isinstance(fix_top, list):
+        fix_top = [fix_top, fix_top]
+    if not isinstance(fix_bottom, list):
+        fix_bottom = [fix_bottom, fix_bottom]
+
+    isfloat = (y.format.sample_type == vs.FLOAT and y.format.bits_per_sample == 32)
+    sep = core.std.SeparateFields(y, True).std.SetFrameProp('_Field', delete=True)
+    st = sep[0::2]
+    sb = sep[1::2]
+    if fix_top[0] > 0 or fix_bottom[0] > 0:
+        st16 = core.resize.Point(st, format=vs.GRAY16, dither_type='error_diffusion')
+        st32 = core.edgefixer.Continuity(st16, top=fix_top, bottom=fix_bottom).resize.Point(format=vs.GRAYS)
+    elif isfloat:
+        st32 = st
+    else:
+        st32 = core.resize.Point(st, format=vs.GRAYS)
+    if fix_top[1] > 0 or fix_bottom[1] > 0:
+        sb16 = core.resize.Point(sb, format=vs.GRAY16, dither_type='error_diffusion')
+        sb32 = core.edgefixer.Continuity(sb16, top=fix_top, bottom=fix_bottom).resize.Point(format=vs.GRAYS)
+    elif isfloat:
+        sb32 = sb
+    else:
+        sb32 = core.resize.Point(sb, format=vs.GRAYS)
+
     if showdiff:
-        downt, difft = _Descale(sep[0::2], width, height, kernel, b, c, taps, src_top[0], src_left[0], True)
-        downb, diffb = _Descale(sep[1::2], width, height, kernel, b, c, taps, src_top[1], src_left[1], True)
+        downt, difft = _Descale(st32, width, height, kernel, b, c, taps, src_left[0], src_top[0], src_width[0], src_height[0], True)
+        downb, diffb = _Descale(sb32, width, height, kernel, b, c, taps, src_left[1], src_top[1], src_width[1], src_height[1], True)
         down = _Weave(downt, downb)
         diff = _Weave(difft, diffb)
         if isfloat:
@@ -36,8 +67,8 @@ def Destripe(clip: vs.VideoNode,
             diffi = core.resize.Point(diff, format=y.format, range_in_s='limited', range_s='full')
             return [downi, diffi]
     else:
-        downt = _Descale(sep[0::2], width, height, kernel, b, c, taps, src_top[0], src_left[0], False)
-        downb = _Descale(sep[1::2], width, height, kernel, b, c, taps, src_top[1], src_left[1], False)
+        downt = _Descale(st32, width, height, kernel, b, c, taps, src_left[0], src_top[0], src_width[0], src_height[0], False)
+        downb = _Descale(sb32, width, height, kernel, b, c, taps, src_left[1], src_top[1], src_width[1], src_height[1], False)
         down = _Weave(downt, downb)
         if isfloat:
             return down
@@ -54,20 +85,22 @@ def _GetDescaler(kernel: str,
                  b: float,
                  c: float,
                  taps: int,
+                 src_left: float,
                  src_top: float,
-                 src_left: float) -> Callable[[vs.VideoNode, int, int], vs.VideoNode]:
+                 src_width: float,
+                 src_height: float) -> Callable[[vs.VideoNode, int, int], vs.VideoNode]:
     if kernel == 'bilinear':
-        return partial(core.descale.Debilinear, src_top=src_top, src_left=src_left)
+        return partial(core.descale.Debilinear, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'bicubic':
-        return partial(core.descale.Debicubic, b=b, c=c, src_top=src_top, src_left=src_left)
+        return partial(core.descale.Debicubic, b=b, c=c, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'lanczos':
-        return partial(core.descale.Delanczos, taps=taps, src_top=src_top, src_left=src_left)
+        return partial(core.descale.Delanczos, taps=taps, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'spline16':
-        return partial(core.descale.Despline16, src_top=src_top, src_left=src_left)
+        return partial(core.descale.Despline16, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'spline36':
-        return partial(core.descale.Despline36, src_top=src_top, src_left=src_left)
+        return partial(core.descale.Despline36, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'spline64':
-        return partial(core.descale.Despline64, src_top=src_top, src_left=src_left)
+        return partial(core.descale.Despline64, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     else:
         raise ValueError('descale: Invalid kernel specified.')
 
@@ -75,20 +108,22 @@ def _GetResizer(kernel: str,
                 b: float,
                 c: float,
                 taps: int,
+                src_left: float,
                 src_top: float,
-                src_left: float) -> Callable[[vs.VideoNode, int, int], vs.VideoNode]:
+                src_width: float,
+                src_height: float) -> Callable[[vs.VideoNode, int, int], vs.VideoNode]:
     if kernel == 'bilinear':
-        return partial(core.resize.Bilinear, src_top=src_top, src_left=src_left)
+        return partial(core.resize.Bilinear, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'bicubic':
-        return partial(core.resize.Bicubic, filter_param_a=b, filter_param_b=c, src_top=src_top, src_left=src_left)
+        return partial(core.resize.Bicubic, filter_param_a=b, filter_param_b=c, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'lanczos':
-        return partial(core.resize.Lanczos, filter_param_a=taps, src_top=src_top, src_left=src_left)
+        return partial(core.resize.Lanczos, filter_param_a=taps, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'spline16':
-        return partial(core.resize.Spline16, src_top=src_top, src_left=src_left)
+        return partial(core.resize.Spline16, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'spline36':
-        return partial(core.resize.Spline36, src_top=src_top, src_left=src_left)
+        return partial(core.resize.Spline36, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     elif kernel == 'spline64':
-        return partial(core.resize.Spline64, src_top=src_top, src_left=src_left)
+        return partial(core.resize.Spline64, src_left=src_left, src_top=src_top, src_width=src_width, src_height=src_height)
     else:
         raise ValueError('resize: Invalid kernel specified.')
 
@@ -99,13 +134,15 @@ def _Descale(clip: vs.VideoNode,
              b: float,
              c: float,
              taps: int,
-             src_top: float,
              src_left: float,
+             src_top: float,
+             src_width: float,
+             src_height: float,
              showdiff: bool) -> Union[vs.VideoNode, List[vs.VideoNode]]:
-    descaler = _GetDescaler(kernel, b, c, taps, src_top, src_left)
+    descaler = _GetDescaler(kernel, b, c, taps, src_left, src_top, src_width, src_height)
     down = descaler(clip, width, height)
     if showdiff:
-        resizer = _GetResizer(kernel, b, c, taps, src_top, src_left)
+        resizer = _GetResizer(kernel, b, c, taps, src_left, src_top, src_width, src_height)
         up = resizer(down, clip.width, clip.height)
         diff = core.std.Expr([clip, up], 'x y - abs')
         return [down, diff]
